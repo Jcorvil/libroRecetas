c00597fca23e343c39b85fc6ac31f5662ff7ab58
unknown <jcorvil600@g.educaand.es>
2024-03-07 11:32:28 +0100

    07/03/2024. Libro receta casi terminado

--- src/app/about/about.page.ts
+++ src/app/about/about.page.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CallNumber } from '@awesome-cordova-plugins/call-number/ngx';
+
+import * as L from 'leaflet';
+
 @Component({
   selector: 'app-about',
   templateUrl: './about.page.html',
@@ -7,9 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutPage implements OnInit {
 
-  constructor() { }
+  constructor(private callNumber: CallNumber) { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  llamadaTel(): void {
+    const phoneNumber = '603551622';
+    this.callNumber
+      .callNumber(phoneNumber, true)
+      .then(() => console.log('Llamando...'))
+      .catch((error) => console.log('Error al iniciar la llamada', error));
+  }
+
+  map: any;
+
+  ionViewDidEnter() {
+    this.loadMap();
+  }
+
+  loadMap() {
+    let latitud = 36.6797047;
+    let longitud = -5.4470656;
+    let zoom = 17;
+    this.map = L.map("mapId").setView([latitud, longitud], zoom);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
+      .addTo(this.map);
   }
 
 }
